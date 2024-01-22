2343f82483dcfe1747801cfb1b3272cc94441a58
Jacrivas <113143264+Jacrivas@users.noreply.github.com>
2024-01-21 22:42:45 -0500

    Snake.ts

--- src/Snake.ts
+++ src/Snake.ts
@@ -10,7 +10,6 @@ class Snake {
     this.color = snakeColor;
   }
   move(moves: number) {
-    display("The", this.color, "snake moves", moves, "step(s)");
     if (this.currentDirection === 1) {
       this.currentPosition = this.currentPosition + moves;
     } else if (this.currentDirection === -1) {
@@ -19,10 +18,8 @@ class Snake {
   }
   turn() {
     if (this.currentDirection === 1) {
-      display("The", this.color, "snake turns backwards");
       this.currentDirection = -1;
     } else if (this.currentDirection === -1) {
-      display("The", this.color, "snake turns forwards");
       this.currentDirection = 1;
     }
   }
